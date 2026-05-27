5b22fc042b7b2083ef24ed357fbb51a3c7b8ee79
hpiening <hpiening@motivsports.com>
2026-05-27 17:44:42 +0100

    allow logins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,44 @@
-import { redirect } from 'next/navigation'
+'use client'
+import { useEffect } from 'react'
+import Script from 'next/script'
 
-// Root redirects to grand-teton as the first live event.
-// When more events are added, this can become an event index page.
+declare global {
+  interface Window {
+    netlifyIdentity: {
+      on: (event: string, cb: (user?: unknown) => void) => void
+    }
+  }
+}
+
 export default function Home() {
-  redirect('/grand-teton')
+  useEffect(() => {
+    const hash = window.location.hash
+    const hasToken =
+      hash.includes('confirmation_token') ||
+      hash.includes('invite_token') ||
+      hash.includes('recovery_token') ||
+      hash.includes('access_token')
+
+    if (!hasToken) {
+      window.location.replace('/grand-teton/')
+    }
+  }, [])
+
+  return (
+    <Script
+      src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+      strategy="afterInteractive"
+      onLoad={() => {
+        if (window.netlifyIdentity) {
+          window.netlifyIdentity.on('init', (user) => {
+            if (!user) {
+              window.netlifyIdentity.on('login', () => {
+                document.location.href = '/admin/'
+              })
+            }
+          })
+        }
+      }}
+    />
+  )
 }
